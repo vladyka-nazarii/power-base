2da05c04490bdf75cd6b508565f137525fb5a04f
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2025-11-16 18:48:40 +0200

    feat(layout): add initial layout

---
+++ app/power-stations/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        Here will be power stations
+    </div>
+  );
+}
---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="border-black/10 border-t h-[40px] flex items-center justify-center px-5">
+            Footer
+        </footer>
+    )
+}
